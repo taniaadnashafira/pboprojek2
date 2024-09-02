d6691736e752bc77e533e015bf571e2fdb5adb67
taniaadnashafira <taniashafiraadna@gmail.com>
2024-09-02 18:44:07 +0700

    commit

---
+++ Taniaproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taniaproject1;
+
+/**
+ *
+ * @author Acer
+ */
+public class Taniaproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Tania Adna Shafira");
+        System.out.println("NPM: 2320506034");
+        System.out.println("Alamat: Bandongan");
+        System.out.println("No Hp: 085718459971");
+    }
+}
